41518220735b19be277c4a2f8e8821a7292577cf
mdriver <mdriver22@gmail.com>
2024-12-17 08:25:22 -0600

    complete listing page

---
+++ src/app/listing/[id]/page.tsx
@@ -0,0 +1,122 @@
+import {
+  FaBath,
+  FaBed,
+  FaChair,
+  FaMapMarkerAlt,
+  FaParking,
+} from "react-icons/fa";
+
+interface ListingParams {
+  params: Promise<{
+    id: string;
+  }>;
+}
+
+interface Listing {
+  name: string;
+  imageUrls: string[];
+  type: "rent" | "sale";
+  regularPrice: number;
+  discountPrice: number;
+  offer: boolean;
+  address: string;
+  description: string;
+  bedrooms: number;
+  bathrooms: number;
+  parking: boolean;
+  furnished: boolean;
+}
+
+export default async function Listing({ params }: ListingParams) {
+  // Await params before using id
+  const { id } = await params;
+  let listing: Listing | null = null;
+
+  try {
+    const result = await fetch(process.env.URL + "/api/listing/get", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ listingId: id }), // Using awaited id here
+      cache: "no-store",
+    });
+    const data = await result.json();
+    listing = data[0];
+  } catch (error) {
+    listing = { name: "Failed to load listing" } as Listing;
+  }
+
+  if (!listing || listing.name === "Failed to load listing") {
+    return (
+      <main className="p-3 flex flex-col max-w-6xl mx-auto min-h-screen">
+        <h2 className="text-xl mt-10 p-3 text-center font-serif max-w-2xl mx-auto lg:text-2xl">
+          Listing not found
+        </h2>
+      </main>
+    );
+  }
+
+  return (
+    <main>
+      <div>
+        <img
+          src={listing.imageUrls[0]}
+          alt={listing.name}
+          className="w-full h-[400px] object-cover"
+        />
+        <div className="flex flex-col max-w-4xl mx-auto p-3 my-7 gap-4">
+          <p className="text-2xl font-semibold">
+            {listing.name} - ${" "}
+            {listing.offer
+              ? (listing.regularPrice - listing.discountPrice).toLocaleString(
+                  "en-US"
+                ) // Show price after discount
+              : listing.regularPrice.toLocaleString("en-US")}
+            {listing.type === "rent" && " / month"}
+          </p>
+          <p className="flex items-center mt-6 gap-2 text-slate-600  text-sm">
+            <FaMapMarkerAlt className="text-green-700" />
+            {listing.address}
+          </p>
+          <div className="flex gap-4">
+            <p className="bg-red-900 w-full max-w-[200px] text-white text-center p-1 rounded-md">
+              {listing.type === "rent" ? "For Rent" : "For Sale"}
+            </p>
+            {listing.offer && (
+              <p className="bg-green-900 w-full max-w-[200px] text-white text-center p-1 rounded-md">
+                ${listing.discountPrice.toLocaleString("en-US")} OFF
+              </p>
+            )}
+          </div>
+          <p className="text-slate-800">
+            <span className="font-semibold text-black">Description - </span>
+            {listing.description}
+          </p>
+          <ul className="text-green-900 font-semibold text-sm flex flex-wrap items-center gap-4 sm:gap-6">
+            <li className="flex items-center gap-1 whitespace-nowrap ">
+              <FaBed className="text-lg" />
+              {listing.bedrooms > 1
+                ? `${listing.bedrooms} beds `
+                : `${listing.bedrooms} bed `}
+            </li>
+            <li className="flex items-center gap-1 whitespace-nowrap ">
+              <FaBath className="text-lg" />
+              {listing.bathrooms > 1
+                ? `${listing.bathrooms} baths `
+                : `${listing.bathrooms} bath `}
+            </li>
+            <li className="flex items-center gap-1 whitespace-nowrap ">
+              <FaParking className="text-lg" />
+              {listing.parking ? "Parking spot" : "No Parking"}
+            </li>
+            <li className="flex items-center gap-1 whitespace-nowrap ">
+              <FaChair className="text-lg" />
+              {listing.furnished ? "Furnished" : "Unfurnished"}
+            </li>
+          </ul>
+        </div>
+      </div>
+    </main>
+  );
+}
